2ae1ca0c57085fe534e4b1d558a352896e76eda9
fmaylinch <ferranmaylinch@gmail.com>
2017-07-30 14:57:02 +0200

    Passing props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const App = function () {
-  //return React.createElement('h1', null, "My first component (normal js)");
-  return <h1>My first component (jsx)</h1>
+  return <Title text="This is a title" color="forestgreen"/>
+}
+
+const Title = function (props) {
+  return <h1 style={{color: props.color}}>{props.text}</h1>
 }
 
 export default App;
