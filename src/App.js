80dfc880627f1c8a0f9541578299b3df5ca77d56
fmaylinch <ferranmaylinch@gmail.com>
2017-07-30 20:11:58 +0200

    Use BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { HashRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Landing from './Landing'
 import About from './About'
 
 const App = () => (
-  <HashRouter>
+  <BrowserRouter>
     <div>
       <Route exact path='/' component={Landing} />
       <Route path='/about' component={About} />
     </div>
-  </HashRouter>
+  </BrowserRouter>
 )
 
 export default App
