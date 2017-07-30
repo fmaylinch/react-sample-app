39dc6d03692c92eda965b472f8b2384f146ede34
fmaylinch <ferranmaylinch@gmail.com>
2017-07-30 16:00:08 +0200

    Simplify arrow functions (one-liners)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-const App = () => {
-  return <Title text='This is a title'/>
-}
+const App = () => <Title text='This is a title'/>
 
-const Title = (props) => {
-  return <h1 className='title'>{props.text}</h1>
-}
+const Title = (props) => <h1 className='title'>{props.text}</h1>
 
-export default App;
+export default App
