be59cf686be1c66dd18a203dd5f1adc9a195e0f9
fmaylinch <ferranmaylinch@gmail.com>
2017-08-04 03:22:56 +0200

    Property initializer syntax for state property also

--- src/Search.js
+++ src/Search.js
@@ -3,13 +3,8 @@ import data from './data.json'
 
 class Search extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = { searchTerm: '' }
-  }
+  state = { searchTerm: '' }
 
-  // property initializer syntax
-  // https://facebook.github.io/react/docs/handling-events.html
   performSearch = (event) => {
     this.setState({ searchTerm: event.target.value })
   }
