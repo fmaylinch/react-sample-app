16bb8714f9f5dac179efa2905bc565754064cafe
fmaylinch <ferranmaylinch@gmail.com>
2017-08-06 01:30:53 +0200

    Add propTypes to MovieCard component

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import './MovieCard.css';
+import './MovieCard.css'
+import { shape, string, number } from 'prop-types'
 
 const MovieCard = (props) => (
   <div className="movie-card">
     <h1>{props.show.name}</h1>
     <img src={"http://image.tmdb.org/t/p/w185" + props.show.poster_path} alt={props.show.name} />
-    <p>{props.show.first_air_date}</p>
+    <p>Score: {props.show.vote_average}</p>
   </div>
 )
+
+MovieCard.propTypes = {
+  show: shape({
+    name: string.isRequired,
+    poster_path: string.isRequired,
+    vote_average: number.isRequired
+  }).isRequired
+}
 
 export default MovieCard
