a1a751021a1d28b0c735dc107da3337a4dccfaa4
fmaylinch <ferranmaylinch@gmail.com>
2017-08-05 21:00:50 +0200

    Use shows (copied from api.themoviedb.org)

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import data from './data.json'
+import shows from './shows.json'
 
 class Search extends Component {
 
@@ -12,13 +12,13 @@ class Search extends Component {
   render() {
     return (
       <div>
-        <h1>Films</h1>
+        <h1 className="title">Shows</h1>
         <input type="text" placeholder="Search"
           value={this.state.searchTerm} onChange={this.performSearch} />
         <ul>
-          { data
-            .filter(film => film.title.toLowerCase().indexOf(this.state.searchTerm) >= 0)
-            .map(film => <li key={film.id}>{film.title}</li>)
+          { shows
+            .filter(show => show.name.toLowerCase().indexOf(this.state.searchTerm) >= 0)
+            .map(show => <li key={show.id}>{show.name}</li>)
           }
         </ul>
       </div>
