cd457cd649b4619be864ec9af31116f5d7897f44
fmaylinch <ferranmaylinch@gmail.com>
2017-08-11 20:19:00 +0200

    Test shallow component (ignore children)

--- src/Search.test.js
+++ src/Search.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer'; // not used, actually
 import Search from './Search';
 
-it('renders correctly', () => {
+it('renders correctly (shallow)', () => {
+  const component = shallow(<Search />)
+  expect(component).toMatchSnapshot()
+});
+
+
+xit('renders correctly (complete)', () => {
   const component = renderer.create(<Search />)
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
