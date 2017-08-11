38bf78a9a1ad4cab65ff593579d76f998937ff61
fmaylinch <ferranmaylinch@gmail.com>
2017-08-11 23:37:05 +0200

    Tests looking for elements, and simulating events

--- src/Search.test.js
+++ src/Search.test.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import renderer from 'react-test-renderer'; // not used, actually
+import renderer from 'react-test-renderer';
 import Search from './Search';
+import MovieCard from './MovieCard';
+import shows from './shows.json'
 
-it('renders correctly (shallow)', () => {
-  const component = shallow(<Search />)
-  expect(component).toMatchSnapshot()
-});
+// required dependencies: yarn add enzyme react-test-renderer
+// run tests: yarn test
+// show coverage: yarn test -- --coverage
+// see report: open coverage/lcov-report/index.html
 
+describe('Search', () => {
 
-xit('renders correctly (complete)', () => {
-  const component = renderer.create(<Search />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-});
+  it('renders correctly (shallow)', () => {
+    const component = shallow(<Search />)
+    // compares render to last snapshot (ignoring children, due to shallow)
+    expect(component).toMatchSnapshot()
+  })
+
+  // Excluded test
+  xit('renders correctly (complete)', () => {
+    const component = renderer.create(<Search />)
+    const tree = component.toJSON()
+    // compares render to last snapshot (including children)
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('displays the correct number of results (unfiltered)', () => {
+    const component = shallow(<Search />)
+    const movieCards = component.find(MovieCard) // finds by component
+    expect(movieCards.length).toEqual(shows.length);
+  })
+
+  it('displays the correct number of results (filtered)', () => {
+    const searchTerm = 'black'
+    const expectedCount = 2
+    const component = shallow(<Search />)
+    const input = component.find('input') // finds by css selector
+    input.simulate('change', {target: {value: searchTerm}}) // simulates a change event, setting 'black' in the input
+    const movieCards = component.find(MovieCard)
+    expect(movieCards.length).toEqual(expectedCount)
+  })
+
+})
