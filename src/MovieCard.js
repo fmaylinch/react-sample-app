f4176e06faaa52d739c30cca6012405f6ca837c9
fmaylinch <ferranmaylinch@gmail.com>
2017-08-12 15:30:19 +0200

    Add flow to MovieCard

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,21 +1,20 @@
+// @flow
+
 import React from 'react'
 import './MovieCard.css'
-import { shape, string, number } from 'prop-types'
 
-const MovieCard = (props) => (
+const MovieCard = (props: {
+  show: {
+    name: string,
+    poster_path: string,
+    vote_average: number
+  }
+}) => (
   <div className="movie-card">
     <h1>{props.show.name}</h1>
     <img src={"http://image.tmdb.org/t/p/w185" + props.show.poster_path} alt={props.show.name} />
     <p>Score: {props.show.vote_average}</p>
   </div>
 )
-
-MovieCard.propTypes = {
-  show: shape({
-    name: string.isRequired,
-    poster_path: string.isRequired,
-    vote_average: number.isRequired
-  }).isRequired
-}
 
 export default MovieCard
