59e5d14d45c951dbbfb3c166681c7697575fa5c4
fmaylinch <ferranmaylinch@gmail.com>
2017-08-19 15:02:06 +0200

    Define Match type in props route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import type { Match } from 'react-router-dom'
 import Landing from './Landing'
 import Search from './Search'
 import About from './About'
@@ -10,6 +11,13 @@ import shows from './shows.json'
 
 const NotFound = () => <h1>Page not found 404</h1>
 
+// we define props type, although flow doesn't complain (maybe infers?)
+const MovieDetailWithShow = (props: {match: Match}) =>
+  <MovieDetail
+    routerProps={props}
+    show={shows.filter(show => show.id === Number(props.match.params.id))[0]}
+  />
+
 const App = () => (
   <BrowserRouter>
     <div>
@@ -17,11 +25,7 @@ const App = () => (
         <Route exact path='/' component={Landing} />
         <Route path='/search' component={Search} />
         <Route path='/about' component={About} />
-        <Route path='/detail/:id' component={
-          (props) => <MovieDetail
-            routerProps={props}
-            show={shows.filter(x => x.id === Number(props.match.params.id))[0]} />
-        } />
+        <Route path='/detail/:id' component={MovieDetailWithShow} />
         <Route component={NotFound} />
       </Switch>
     </div>
